45c6c62e0ece9b6cfc20930ceb954ebaa0fcd30f
aipdamary <arifsulthan99@gmail.com>
2022-10-26 19:44:22 +0700

    first

---
+++ app/src/main/java/com/example/logintugasandroid/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.logintugasandroid;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/logintugasandroid/loginActivity.java
@@ -0,0 +1,16 @@
+package com.example.logintugasandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
